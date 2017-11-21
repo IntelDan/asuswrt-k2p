6771bd4a38209040f11e33356024e7f7d7ea4b15
edward-p <edward@MSI-GP62>
2017-11-21 10:43:31 +0800

    add mentohust_mon.sh

---
+++ release/src/router/myscript/usr/sbin/mentohust_mon.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+check_time=120
+enabled=$(nvram get mentohust_enabled)
+if [ "$enabled" != "1" ]
+then
+	exit 0
+fi
+#start daemon
+while [ "1" = "1" ]  
+do 
+  sleep $check_time
+#check mentohust
+
+   icount=`ps -w | grep "mentohust -b3" | grep -v grep |wc -l`
+
+   if [ $icount = 0 ] ;then
+   logger -t "mentohust" "mentohust error,restart mentohust!"
+   /usr/sbin/myscript_mentohust.sh stop
+   /usr/sbin/myscript_mentohust.sh start
+   exit 0
+   fi
+
+done
--- release/src/router/myscript/usr/sbin/myrcs.sh
+++ release/src/router/myscript/usr/sbin/myrcs.sh
@@ -3,6 +3,6 @@
 /sbin/modprobe xt_set
 /sbin/modprobe ip_set_hash_net
 /sbin/modprobe ip_set_hash_ip
-/usr/sbin/myscript_mentohust.sh start
+/usr/sbin/mentohust_mon.sh &
 /usr/sbin/ssr_mon.sh &
 #nvram set country_code=#a
